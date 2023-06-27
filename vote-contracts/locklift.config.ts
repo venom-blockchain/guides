6d8312536be31fb3454d8a4767540267c26cb11e
1 <a1@MacBook-Pro-1.local>
2023-06-27 09:10:53 +0200

    upd vote-contracts env

--- vote-contracts/locklift.config.ts
+++ vote-contracts/locklift.config.ts
@@ -1,6 +1,5 @@
 import { LockliftConfig } from "locklift";
 import { FactorySource } from "./build/factorySource";
-import { SimpleGiver, GiverWallet } from "./giverSettings";
 
 declare global {
   const locklift: import("locklift").Locklift<FactorySource>;
@@ -46,7 +45,6 @@ const config: LockliftConfig = {
       // This giver is default local-node giverV2
       giver: {
         // Check if you need provide custom giver
-        giverFactory: (ever, keyPair, address) => new SimpleGiver(ever, keyPair, address),
         address: "0:ece57bcc6c530283becbbd8a3b24d3c5987cdddc3c8b7b33be6e4a6312490415",
         key: "172af540e43a524763dd53b26a066d472a97c4de37d5498170564510608250c3",
       },
@@ -62,11 +60,10 @@ const config: LockliftConfig = {
     },
     mainnet: {
       // Specify connection settings for https://github.com/broxus/everscale-standalone-client/
-      connection: "mainnet",
+      connection: "mainnetJrpc",
       // This giver is default Wallet
       giver: {
         // Check if you need provide custom giver
-        giverFactory: (ever, keyPair, address) => new GiverWallet(ever, keyPair, address),
         address: "0:ece57bcc6c530283becbbd8a3b24d3c5987cdddc3c8b7b33be6e4a6312490415",
         key: "172af540e43a524763dd53b26a066d472a97c4de37d5498170564510608250c3",
       },
